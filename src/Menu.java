96a20a38c4e84f993d14409a96d29bca99333070
Gonzalo04 <gonzalosv88@hotmail.com>
2016-03-16 18:22:21 +0100

    Clases Principal y Busqueda

--- src/Menu.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Gonzalo on 16/03/2016.
- */
-public class Menu {
-}
--- src/Principal.java
+++ src/Principal.java
@@ -3,8 +3,10 @@ public class Principal {
 
     public static void main (String[] args ){
 
-        System.out.println("Hola this is a Principal Class");
+        Busqueda miBusqueda = new Busqueda("Mi primer TFG");
 
+
+        miBusqueda.buscar();
     }
 
 }
